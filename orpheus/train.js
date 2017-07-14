274902c628ac685319841b5a5cc8547ad8cc8d97
Shun Yu Rao <shunyu.rao@gmail.com>
2017-07-14 00:38:12 +0000

    Use reaction to song to train model

--- orpheus/train.js
+++ orpheus/train.js
@@ -6,16 +6,15 @@ const conf = require('../config');
 let mod = module.exports = {};
 
 mod.trainModel = function(token, features, verdict, callback) {
-  let py = spawn('python3', ['orpheus/ml_scripts/test.py']);
+  let py = spawn('python3', ['orpheus/ml_scripts/test.py', token, JSON.stringify(features), verdict]);
 
   py.stdout.on('data', function(data) {
     console.log(data.toString());
   });
 
   py.stdout.on('end', function() {
-    console.log("end");
+    callback();
   });
-
-  py.stdin.write(JSON.stringify(features));
+  
   py.stdin.end();
 };
